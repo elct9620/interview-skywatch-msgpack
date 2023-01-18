a46c41a559b03ba56ad6a413292687e57b8db8ca
蒼時弦也 <elct9620@frost.tw>
2023-01-18 18:48:45 +0800

    Refactor to recursive encode nested data

--- pkg/msgpack/encode.go
+++ pkg/msgpack/encode.go
@@ -11,27 +11,39 @@ func Marahal(v any) ([]byte, error) {
 		buffer = append(buffer, TypeNil)
 	} else {
 		va := reflect.ValueOf(v)
-
-		switch va.Kind() {
-		case reflect.String:
-			buffer = append(buffer, encodeString(va)...)
-		case reflect.Map:
-			elements := va.Len()
-			buffer = append(buffer, byte(TypeFixMap)|byte(elements))
-			for _, key := range va.MapKeys() {
-				buffer = append(buffer, encodeString(key)...)
-				buffer = append(buffer, encodeString(va.MapIndex(key))...)
-			}
-		}
+		buffer = append(buffer, encode(va)...)
 	}
 
 	return buffer, nil
+}
+
+func encode(va reflect.Value) (buffer []byte) {
+	switch va.Kind() {
+	case reflect.String:
+		buffer = append(buffer, encodeString(va)...)
+	case reflect.Map:
+		buffer = append(buffer, encodeMap(va)...)
+	}
+
+	return buffer
 }
 
 func encodeString(va reflect.Value) (buffer []byte) {
 	strLen := va.Len()
 	buffer = append(buffer, byte(TypeFixStr)|byte(strLen))
 	buffer = append(buffer, []byte(va.String())...)
+
+	return buffer
+}
+
+func encodeMap(va reflect.Value) (buffer []byte) {
+	elements := va.Len()
+	buffer = append(buffer, byte(TypeFixMap)|byte(elements))
+	for _, key := range va.MapKeys() {
+		buffer = append(buffer, encodeString(key)...)
+
+		buffer = append(buffer, encode(va.MapIndex(key))...)
+	}
 
 	return buffer
 }
