f16ded945a00525b98c2f200197e5422343cbe91
蒼時弦也 <elct9620@frost.tw>
2023-01-18 18:44:26 +0800

    Add FixMap support (string only)

--- pkg/msgpack/encode.go
+++ pkg/msgpack/encode.go
@@ -14,11 +14,24 @@ func Marahal(v any) ([]byte, error) {
 
 		switch va.Kind() {
 		case reflect.String:
-			strLen := va.Len()
-			buffer = append(buffer, byte(TypeFixStr)|byte(strLen))
-			buffer = append(buffer, []byte(va.String())...)
+			buffer = append(buffer, encodeString(va)...)
+		case reflect.Map:
+			elements := va.Len()
+			buffer = append(buffer, byte(TypeFixMap)|byte(elements))
+			for _, key := range va.MapKeys() {
+				buffer = append(buffer, encodeString(key)...)
+				buffer = append(buffer, encodeString(va.MapIndex(key))...)
+			}
 		}
 	}
 
 	return buffer, nil
+}
+
+func encodeString(va reflect.Value) (buffer []byte) {
+	strLen := va.Len()
+	buffer = append(buffer, byte(TypeFixStr)|byte(strLen))
+	buffer = append(buffer, []byte(va.String())...)
+
+	return buffer
 }
--- pkg/msgpack/types.go
+++ pkg/msgpack/types.go
@@ -3,4 +3,5 @@ package msgpack
 const (
 	TypeNil    = 0xc0
 	TypeFixStr = 0b10100000
+	TypeFixMap = 0b10000000
 )
